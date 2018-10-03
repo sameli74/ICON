42ef6a4e80ee138d09c80a2675ec1dc39f5c186b
sameli <sameli@scspc242.cs.uwaterloo.ca>
2018-10-03 17:34:41 -0400

    #define

--- SPI.c
+++ SPI.c
@@ -4,12 +4,16 @@
 #include <wiringPi.h>
 #include <time.h>
 
+#define -1 SETUP_ERROR
+
 int main(){
 
 	if (wiringPiSetupGpio())
 		printf("Couldn't initialize\n");
-	else
-		printf("Initialization was successful");
+		else{
+			printf("Initialization was successful\n");
+			return
+		}
 
 
 	return 0;
