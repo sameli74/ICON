07133342210859ed1ca895bf8756563099cb891e
sameli <sameli@scspc242.cs.uwaterloo.ca>
2018-10-04 19:24:06 -0400

    edit

--- C/SPI.c
+++ C/SPI.c
@@ -6,16 +6,16 @@
 
 #define  SETUP_ERROR -1
 const int CHANNEL=0;
-const int SPEED=100000;
+const int SPEED=500000;
 
 int main(){
 	int status;
 	unsigned char data[1];
 	data[0]= 0xAA;
-	if (wiringPiSetupGpio()){
-		printf("Couldn't initialize\n");
-		return SETUP_ERROR;
-	}
+	// if (wiringPiSetupGpio()){
+	// 	printf("Couldn't initialize\n");
+	// 	return SETUP_ERROR;
+	// }
 status= wiringPiSPISetup(CHANNEL, SPEED);
 if(status==-1){
 	printf("something wrong happened!\n");
