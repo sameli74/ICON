748cf8926948ba95e24e0bf6928dd9443d4061cd
sameli <sameli@scspc242.cs.uwaterloo.ca>
2018-10-04 18:44:43 -0400

    SPI coding

--- C/SPI.c
+++ C/SPI.c
@@ -5,15 +5,27 @@
 #include <time.h>
 
 #define  SETUP_ERROR -1
-
+const int CHANNEL=0;
+const int SPEED=500000;
 
 int main(){
-
+	int status;
+	int data= 0xAA;
 	if (wiringPiSetupGpio()){
 		printf("Couldn't initialize\n");
 		return SETUP_ERROR;
 	}
-
+printf("Initializing...\n");
+status= wiringPiSPISetup(CHANNEL, SPEED);
+if(status==-1){
+	printf("something wrong happened!\n");
+	return SETUP_ERROR;
+}
+while(1){
+	wiringPiSPIDataRW(CHANNEL, data);
+	delayMicroseconds(100);
+}
+return 0;
 
 
 
